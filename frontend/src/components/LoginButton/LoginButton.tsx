0a1b0cddec86baf5d3747ffeec62ac03858bb6b5
lanceBarnes <ltbarnes@ucdavis.edu>
2025-04-20 07:39:01 -0700

    wip2

--- frontend/src/components/LoginButton/LoginButton.tsx
+++ frontend/src/components/LoginButton/LoginButton.tsx
@@ -2,7 +2,7 @@ import { Button, Tooltip, UnstyledButton } from '@mantine/core';
 import { useAuth0 } from '@auth0/auth0-react';
 import { IconSwitchHorizontal } from '@tabler/icons-react';
 import classes from '../Nav/Nav.module.css';
-export function LoginButton({active}:{active:boolean}) {
+export function LoginButton({active}:{active?:boolean}) {
   const { loginWithRedirect } = useAuth0();
 
   return (
