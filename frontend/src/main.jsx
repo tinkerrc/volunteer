7dfedbf8fce7b160650bd2e50867c09a6dfa5f25
Zhenkai Weng <hello@zhenkai.dev>
2025-04-19 23:37:54 -0700

    refactor: rename (#31)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // optional global styles
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Dashboard from './dashboard.jsx';
-import VolunteerPage from './volpage.jsx';
+import Dashboard from './Dashboard.jsx';
+import './index.css'; // optional global styles
+import VolunteerPage from './VolunteerPage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,23 +14,12 @@ const router = createBrowserRouter([
     path: '/volunteer',
     element: <VolunteerPage />,
   },
-  
+
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
-// function App() {
-//   const [currentPage, setCurrentPage] = useState("home");
-
-//   const goto = (page) => setCurrentPage(page);
-
-//   if (currentPage === "home") return <Dashboard goto={goto} />;
-//   if (currentPage === "vol") return <Vol goto={goto} />;
-//   if (currentPage === "login") return <Login goto={goto} />;
-
-//   return <div>Page not found</div>;
-// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
