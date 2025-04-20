2bbb1a14297ab8f77626c34a8a38a58127e057b7
lanceBarnes <ltbarnes@ucdavis.edu>
2025-04-20 07:37:16 -0700

    wip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(<Auth0Provider
+    domain="dev-71ch67bjahab7qg3.us.auth0.com"
+    clientId="kDMwJpEeqPfwTXNaZA1mrKg2juQiTylR"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>,);
