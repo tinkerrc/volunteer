5854e8591fe8e8641127b89c16cb8076aacf8438
Zhenkai Weng <hello@zhenkai.dev>
2025-04-20 12:18:51 +0000

    uncomment events

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { EventList } from './components/EventList/EventList';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
-  // {
-  //   path: '/events',
-  //   element: <EventList />,
-  // },
+  {
+    path: '/events',
+    element: <EventList />,
+  },
 ]);
 
 export function Router() {
