1ad149e76ebae84abbdad0b8efa3cf26d4bb5979
Zhenkai Weng <hello@zhenkai.dev>
2025-04-20 17:44:17 +0000

    fix

--- frontend/src/components/TableSort/CreateVolunteerForm.tsx
+++ frontend/src/components/TableSort/CreateVolunteerForm.tsx
@@ -53,8 +53,9 @@ export const CreateVolunteerForm = () => {
     }
 
     return (
-        <Center bg="var(--mantine-color-gray-light)">
+        <Center maw="600" mb="70" mt="70" bg="var(--mantine-color-gray-light)">
             <form onSubmit={form.onSubmit(submitForm)}>
+
                 <TextInput withAsterisk label="Email" placeholder='your@email.com' key={form.key('email')} {...form.getInputProps('email')} />
                 <TextInput withAsterisk label="First Name" placeholder='Jane' key={form.key('firstName')} {...form.getInputProps('firstName')} />
                 <TextInput label="Middle Name" placeholder='Jane' key={form.key('middleName')} {...form.getInputProps('middleName')} />
