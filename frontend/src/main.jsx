fe4ed92d3699e32c2f7ed1aecb30629e651978c3
Lance Barnes <131405958+LanceB23@users.noreply.github.com>
2025-04-19 23:31:08 -0700

    feat: routing (#30)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css'; // optional global styles
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from './dashboard.jsx';
+import VolunteerPage from './volpage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/volunteer',
+    element: <VolunteerPage />,
+  },
+  
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+// function App() {
+//   const [currentPage, setCurrentPage] = useState("home");
+
+//   const goto = (page) => setCurrentPage(page);
+
+//   if (currentPage === "home") return <Dashboard goto={goto} />;
+//   if (currentPage === "vol") return <Vol goto={goto} />;
+//   if (currentPage === "login") return <Login goto={goto} />;
+
+//   return <div>Page not found</div>;
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
