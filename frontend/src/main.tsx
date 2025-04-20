da2440b069249f2a0f09a40c13764e1ee56ad12f
lanceBarnes <ltbarnes@ucdavis.edu>
2025-04-20 07:53:06 -0700

    bigger fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(<Auth0Provider
     domain="dev-v8cbdhmtmu4lj338.us.auth0.com"
     clientId="xtR3z01pmtfqLnsC2Y2Vjl25pLTPDkQC"
     authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
+        redirect_uri: window.location.origin,
+        audience: "https://api.yolovms.org/",
+        scope: "read:current_user update:current_user_metadata"
+      }}
   >
     <App />
   </Auth0Provider>,);
