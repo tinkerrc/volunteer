4d4c2c78573d0ce2e26cd7624be6abd40f5dcb38
Zhenkai Weng <hello@zhenkai.dev>
2025-04-20 09:09:35 +0000

    chore: mantine template

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,33 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import mantine from 'eslint-config-mantine';
+import tseslint from 'typescript-eslint';
 
-export default [
-  { ignores: ['dist'] },
+export default tseslint.config(
+  ...mantine,
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
   {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+    files: ['**/*.story.tsx'],
+    rules: { 'no-console': 'off' },
+  }
+);
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,28 +1,4 @@
-import '@mantine/core/styles.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { EventList } from './EventList';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { MantineProvider } from '@mantine/core';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <EventList />,
-  },
-  // {
-  //   path: '/volunteer',
-  //   element: <VolunteerPage />,
-  // },
-]);
-
-function App() {
-  return <MantineProvider><RouterProvider router={router} /></MantineProvider>;
-}
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
