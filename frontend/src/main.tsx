b301e348fc79819a467b9ece8017eab4ffbd1cd3
lanceBarnes <ltbarnes@ucdavis.edu>
2025-04-20 07:46:42 -0700

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Auth0Provider
-    domain="dev-71ch67bjahab7qg3.us.auth0.com"
-    clientId="kDMwJpEeqPfwTXNaZA1mrKg2juQiTylR"
+    domain="dev-v8cbdhmtmu4lj338.us.auth0.com"
+    clientId="xtR3z01pmtfqLnsC2Y2Vjl25pLTPDkQC"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
