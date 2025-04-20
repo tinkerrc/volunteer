8fe8840baa3005f79a038e9335d1248114e541a4
Zhenkai Weng <hello@zhenkai.dev>
2025-04-20 12:33:05 +0000

    fix: allowed hosts

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -9,4 +9,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
+  server: { allowedHosts: ["127.0.0.1", "yolovms.org"] },
 });
